8105cbee0f922442ee7f3d907943291394282b84
Harsh Vardhan Pancholi <36657761+HarshVardhanPancholi@users.noreply.github.com>
2021-06-23 14:20:12 +0530

    Update ValidateUsername.java

--- regex/ValidateUsername.java
+++ regex/ValidateUsername.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
+/***********************Conditions*********************/
+/*
+===>The username consists of  to  characters inclusive.
+    If the username consists of less than  or greater than  characters,
+    then it is an invalid username.
+===>The username can only contain alphanumeric characters and underscores (_).
+    Alphanumeric characters describe the character set consisting of lowercase characters ,
+    uppercase characters , and digits .
+===>The first character of the username must be an alphabetic character,
+    i.e., either lowercase character  or uppercase character .
+ */
+
 class UsernameValidator {
     public static final String regex = "^[a-zA-Z]([a-zA-Z0-9_]){7,19}";
 }
-
-
 public class ValidateUsername { 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
