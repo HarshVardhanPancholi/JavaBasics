0171c523b665a9d3f4c85ddbd7490380b58540cc
Harsh Vardhan Pancholi <36657761+HarshVardhanPancholi@users.noreply.github.com>
2021-06-23 12:50:27 +0530

    Create matches.java

---
+++ String_Tokens/matches.java
@@ -0,0 +1,21 @@
+public class Demo {
+ 
+  public static void main(String[] args) {
+          Scanner scan = new Scanner(System.in);
+          String s = scan.nextLine();
+          String a[]=s.split("[^a-zA-Z]");
+          int count=0;
+          for(String h:a){
+              if((h.matches("[a-zA-Z]+"))){
+               count++;   
+              }
+          }
+          System.out.println(count);
+          for(String h:a){
+              if(!(h.equals(" "))){
+                  System.out.println(h);
+              }
+          }
+          scan.close();
+      }
+}
